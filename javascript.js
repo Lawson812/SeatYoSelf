fc6d1d31dd656650f6e630b1c58f6bd8b64156b3
James <lawson.james.e@gmail.com>
2017-04-25 20:49:06 -0400

    hover

---
+++ javascript.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    var seat = $('.seat');
+
+$('.seat').on('mouseover',function(){
+    $(this).css('opacity', '0.7');
+});
+$('.seat').on('mouseleave',function(){
+    $(this).css('opacity','1');
+});
+    var formObjects ={name:"",
+                      phone:"",
+                      age:"",
+                      email:"",
+                      movie:""};
+
+    
+
+
+
+
+
+
+
+});
