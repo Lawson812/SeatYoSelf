f864ae5713dc11ddc8d152d49bfaf730429904b5
James <lawson.james.e@gmail.com>
2017-04-27 22:05:03 -0400

    added comments

--- javascript.js
+++ javascript.js
@@ -4,89 +4,71 @@ $(document).ready(function(){
     $("#clickhere").click(function(event) {
     $("#shows").slideToggle('slow');
   });
+
 //change opacity of seats on hover
-    
-$('.seat.available').on('mouseover',function(){
-// slideDown movies
-    $("#clickhere").click(function(event) {
-    $("#shows").slideToggle('slow');
-  });
-
-// seat hover functions
-
-$('.seat').on('mouseover',function(){
+//mouseover function
+$('.available').on('mouseover',function(){
     $(this).css('opacity', '0.7');
 });
 $('.seat.available').on('mouseleave',function(){
     $(this).css('opacity','1');
 });
-
-    var formObjects ={name:"",
-                      phone:"",
-                      age:"",
-                      email:"",
-                      movie:""};
-                      
+//declare empty array to place reservation objects 
+        var reservations=[];    
 //Make the seats click and bring down the form modal
-    $('.seat').click(function(){
 
-
-    
-    var availableSeats =[];
-    var takenSeats =[];
-    
-//seats click events
-    $('.seat.available').click(function(){
-       
-        var seat=$(this).id();
+$('.available').on('click', function(e){
+  var seatNum = e.target.innerText;
+  var thisthing= e.target;
+  console.log(seatNum);
      $('#formModal').css('display','block');
      $('#infoForm').slideDown(500);
        $('.close').on('click',function(){
            $('#formModal').css('display','none');
        });
-     
+//declared empty object 'person' to place form values inside of, to be later cataloged in the 'reservations' array
+       var person ={name:"",
+       email: "",
+       age:"",
+       phone:"",
+       movie:"",
+       seat:""};
+    //onclick events for reserve button in form modal
+        $('.reserveButton').on('click',function(){
+//onclick, inputs out the values of 'person' object with form data
+          person.name=$('#name').val();
+          person.email=$('#email').val();
+          person.age=$('#age').val()
+          person.phone=$('#phone').val();
+          person.movie=$('#movieName');
+          person.seat=seatNum;
+//unshift places new person object into reservations array at [0]/
+          reservations.unshift(person);
+//each jQuery loop that iterates through all elements with classes both 'seat' and 'available', then iterates through the reservations array using a forEach loop and finds the inner text of the items with seat and available classes, and compares them with the seat objects in the reservation array and if they match, removes the available class, adds the taken class, and removes the click event. then appends the name value at reservations[0] to the targeted div. also changes display of modal to none 
+              $('.seat.available').each(function(x,num){
+                reservations.forEach(function(thisSeat){
 
-        $('.reserveButton').on('click',function (){
-            var name = $('#name').val();
-            var email = $('#email').val();
-            var age= $('#age').val();
-            var phone=$('#phone').val();
-<<<<<<< HEAD
-            var movieName=$('#movieName').val();                       
-
-            seat.setAttribute('class', 'seat taken')
-            
-            $('#formModal').css('display','none');
-        });
-=======
-            var movieName=$('#movieName').val();
-            var seat=$('.seat');
-            reservations[0]={name:name, email:email, age:age, phone:phone,movie:movieName};
-
-            var reservationInfo = document.createElement('p');
-            var thisSeat=document.getElementById;
-            reservationInfo.innerText ="Name: " + name;
-            thisSeat.appendChild(reservationInfo);
+                  if(num.innerText===thisSeat.seat){
+                    $(num).removeClass('available').addClass('taken').off('click');
+                     thisthing.append(reservations[0].name);
+                     }
+                  $('#formModal').css('display','none');
+                });
+              })
+           });
+      }); //end of click
 
 
-            $('#formModal').css('display','none');
-
-        });
 
 
-});
 
 
-});
 
 //back to top button
     var infoArray=[];
     $('.backToTop').click(function(){
         $('html, body').animate({
     scrollTop: ($('.header').offset().top)
-},500);
+    },500);
     });
-
 });
-
-
